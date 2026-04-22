284790670e490b23e0e29e06897bd974f5d94c37
Moises <m.jimenez.giron@iescristobaldemonroy.es>
2026-04-22 17:43:51 +0200

    Añadida documentación JavaDoc

--- Holamundo/src/HolaMundoJava.java
+++ Holamundo/src/HolaMundoJava.java
@@ -3,7 +3,10 @@ public class HolaMundoJava {
 
 	public static void main(String[] args) {
 		
-
+/**
+ * Clase creada para la practica de Entornos de Desarrollo.
+ * @author Moises
+ */
 		System.out.println("Hola a todos");
 
 			
